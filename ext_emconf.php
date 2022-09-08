ccaaeeb7c4e143f6baf944c5147fb8dcc5b3889d
Programmierschlampe <office@ids.co.at>
2022-09-08 13:03:27 +0200

    init

---
+++ ext_emconf.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * TYPO3 Extension "Sitepackage for website www.veridat.io"
+ *
+ * Author: Ing. Gerhard Huber <ids.co.at>
+ * https://ids.co.at/
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Sitepackage for website www.veridat.io',
+    'description' => 'Site Package with Scripts, Templates and Resources ',
+    'category' => 'fe',
+    'version' => '1.0.0',
+    'state' => 'stable',
+    'clearcacheonload' => true,
+    'author' => 'Ing. Gerhard Huber <ids.co.at>',
+    'author_email' => 'office@ids.co.at',
+    'author_company' => 'ids.co.at',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.4.0-7.4.99',
+            'typo3' => '11.0.0-11.5.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ]
+    ]
+];
---
+++ ext_localconf.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * TYPO3 Extension "Sitepackage for website www.veridat.io"
+ *
+ * Author: Ing. Gerhard Huber <ids.co.at>
+ * https://ids.co.at/
+ */
+
+defined('TYPO3_MODE') or die();
+
